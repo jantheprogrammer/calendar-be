c05244deded696898d674e6f88d5cbbfaee9a946
Jan <jantheprogrammer@gmail.com>
2020-01-04 14:09:14 -0800

    Test with no token sended on server

--- google_cal/events.py
+++ google_cal/events.py
@@ -5,7 +5,7 @@ import datetime
 def get_events():
     service = setup_cal.get_calendar()
 
-    # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     # timeMin=now *in events().list
     # pylint: disable=no-member
     events_result = service.events().list(calendarId='primary',
@@ -13,8 +13,8 @@ def get_events():
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
 
+    # print(events)
+    # print('+++'*10)
+    # print(now)
+
     return events
-
-
-if __name__ == '__main__':
-    get_events()
