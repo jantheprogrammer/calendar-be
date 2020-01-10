b2b7c08ceb6f7a434c9380e730de4b7e65c33840
Jan Krejca <Jan.Krejca@ilac.com>
2020-01-10 12:11:06 -0800

    CORS problem

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from google_cal import events
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 
 @app.route('/api', methods=['GET'])
