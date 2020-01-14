1e0acbbc314de5f3990b800049495d3777ba708b
Jan Krejca <Jan.Krejca@ilac.com>
2020-01-14 10:55:37 -0800

    Date arguments

--- google_cal/events.py
+++ google_cal/events.py
@@ -6,22 +6,24 @@ def calculate_length(start, end):
     start_time = start.split('T')[1][:5]
     end_time = end.split('T')[1][:5]
 
-    start_time_m = int(start_time.split(':')[0]) * 60 + int(start_time.split(':')[1])
+    start_time_m = int(start_time.split(
+        ':')[0]) * 60 + int(start_time.split(':')[1])
     end_time_m = int(end_time.split(':')[0]) * 60 + int(end_time.split(':')[1])
-    
+
     return end_time_m - start_time_m
 
 
 def format_events(events):
     formated_events = []
     for event in events:
-        formated_event = {'summary': event['summary'], 'description': event['description'], 'id': event['id']}
+        formated_event = {
+            'summary': event['summary'], 'description': event['description'], 'id': event['id']}
         try:
             start = event['start']['dateTime']
             end = event['end']['dateTime']
             date = event['start']['dateTime'].split('T')[0][:10]
             length = calculate_length(start, end)
-            
+
             formated_event['date'] = date
             formated_event['length'] = length
         except:
@@ -30,17 +32,16 @@ def format_events(events):
 
         formated_events.append(formated_event)
     return formated_events
-    
 
 
-def get_events():
+def get_events(from_date, to_date):
     service = setup_cal.get_calendar()
 
     # now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
     # timeMin=now *in events().list
     # pylint: disable=no-member
-    events_result = service.events().list(calendarId='primary', timeMin='2020-01-01T00:00:00.0Z',
-                                          maxResults=10, singleEvents=True,
+    events_result = service.events().list(calendarId='primary', timeMin=from_date, timeMax=to_date,
+                                          maxResults=100, singleEvents=True,
                                           orderBy='startTime').execute()
     events = events_result.get('items', [])
     formated_events = format_events(events)
